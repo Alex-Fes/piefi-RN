728d09973661824428f2581b84435872ff7384bd
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-14 14:11:04 +0100

    add constants.ts

---
+++ src/constants/constants.ts
@@ -0,0 +1,9 @@
+import {Dimensions} from "react-native";
+
+const {width, height} = Dimensions.get('screen')
+
+const WIDTH = width
+
+const HEIGHT = height
+export const PADDING = 15
+export const NUM_COLUMN = 2
