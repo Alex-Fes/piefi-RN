589c12d092e680d57b773310ace588c0e965795e
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-14 21:41:00 +0100

    ESLint

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,34 +1,50 @@
-import React from 'react';
-import {TouchableOpacity, Text, StyleSheet, TouchableOpacityProps} from "react-native";
+import React from 'react'
+
+import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native'
 
 type Props = {
-    title: string
-    fontSize: number
-    fontWeight: "normal" | "bold" | "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900" | undefined
-    color: string
-    children?: React.ReactNode
+  title: string
+  fontSize: number
+  fontWeight:
+    | 'normal'
+    | 'bold'
+    | '100'
+    | '200'
+    | '300'
+    | '400'
+    | '500'
+    | '600'
+    | '700'
+    | '800'
+    | '900'
+    | undefined
+  color: string
+  children?: React.ReactNode
 } & TouchableOpacityProps
 
-export const Button = ({title, color, fontSize, fontWeight, children, ...restProps}: Props) => {
-    return (
-        <TouchableOpacity {...restProps}>
-            <Text style={[styles.title, {
-                fontSize,
-                fontWeight,
-                color
-            }]}>{title}{children}</Text>
-        </TouchableOpacity>
-    )
-};
+export const Button = ({ title, color, fontSize, fontWeight, children, ...restProps }: Props) => {
+  return (
+    <TouchableOpacity {...restProps}>
+      <Text
+        style={[
+          styles.title,
+          {
+            fontSize,
+            fontWeight,
+            color,
+          },
+        ]}
+      >
+        {title}
+        {children}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 const styles = StyleSheet.create({
-    button: {},
-    title: {
-        textAlign: 'center',
-
-        // justifyContent: "center",
-        // alignItems: "center",
-        // flexDirection: "row",
-    },
-});
-
+  button: {},
+  title: {
+    textAlign: 'center',
+  },
+})
