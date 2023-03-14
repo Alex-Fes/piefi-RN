f3ef23dcd7be76fefefc45e080807f3fc1e6e76a
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-14 21:43:14 +0100

    ESLint

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import {RootStackParamList} from "../types/navigationsTypes";
-import {StyleSheet} from "react-native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {Onboard} from "./Onboard/Onboard";
+import React from 'react'
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { RootStackParamList } from '../types/navigationsTypes'
+
+import { Onboard } from './Onboard/Onboard'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
+
 export const Main = () => {
-    return (
-        // <View style={styles.container}>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                {/*<Stack.Screen name={'Splash'} component={Splash}/>*/}
-                <Stack.Screen name={'Onboard'} component={Onboard}/>
-            </Stack.Navigator>
-        // </View>
-    );
-};
+  return (
+    // <View style={styles.container}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/*<Stack.Screen name={'Splash'} component={Splash}/>*/}
+      <Stack.Screen name={'Onboard'} component={Onboard} />
+    </Stack.Navigator>
+    // </View>
+  )
+}
 
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-
-    },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+// })
