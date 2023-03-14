5c2403a71a2870adc37a2a76f1e52696bb83b14c
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-14 21:46:33 +0100

    add ESLint

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import {StyleSheet, View} from "react-native";
-import {MainLogo} from "../svgIcons/MainLogo";
-import {Button} from "../components/Button";
-import {useAppNavigation} from "../types/navigationsTypes";
+import React from 'react'
+
+import { StyleSheet, View } from 'react-native'
+
+import { Button } from '../components/Button'
+import { MainLogo } from '../svgIcons/MainLogo'
+import { useAppNavigation } from '../types/navigationsTypes'
 
 export const Splash = () => {
-    const navigation = useAppNavigation()
-    return (
-        <View style={styles.container}>
-            <Button
-                style={styles.button}
-                title={''}
-                fontSize={20}
-                fontWeight={undefined}
-                color={''}
-                onPress={() => {
-                    navigation.navigate('Onboard')
-                }}>
-                <MainLogo/>
-            </Button>
-        </View>
-    );
-};
+  const navigation = useAppNavigation()
 
+  return (
+    <View style={styles.container}>
+      <Button
+        style={styles.button}
+        title={''}
+        fontSize={20}
+        fontWeight={undefined}
+        color={''}
+        onPress={() => {
+          navigation.navigate('Onboard')
+        }}
+      >
+        <MainLogo />
+      </Button>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        background: '#FFF',
-        borderRadius: 16,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    button: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    }
-});
+  container: {
+    flex: 1,
+    background: '#FFF',
+    borderRadius: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
