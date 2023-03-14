c6b77c8a0bd819223de3d4f9f3372236b5af47bc
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-14 21:40:37 +0100

    ESLint

--- App.tsx
+++ App.tsx
@@ -1,25 +1,25 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, View} from 'react-native';
-import {Main} from "./src/screens/Main";
-import {NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+
+import { Main } from './src/screens/Main'
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <View style={styles.container}>
-                <Main/>
-                <StatusBar style="auto"/>
-            </View>
-        </NavigationContainer>
-
-    );
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Main />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-    },
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+})
