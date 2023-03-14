6787a81a9f95f67904bccfa8c97181a939ff3e27
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-14 14:08:02 +0100

    change styles, add button

--- src/screens/Onboard/Onboard.tsx
+++ src/screens/Onboard/Onboard.tsx
@@ -1,24 +1,37 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, ImageBackground} from "react-native";
+import {View, Text, StyleSheet, ImageBackground, Image} from "react-native";
+import {Button} from "../../components/Button";
+
 
 
 type ViewType = 'first' | 'second'
 
 export const Onboard = () => {
     const [view, setView] = useState<ViewType>('first')
+    const [language, setLanguage] = useState('RU')
 
-    const image = view === 'first'
-        ? {uri: require('../../../assets/Mesh-gradient.png')}
-        : {uri: require('../../../assets/Mesh-gradient1.png')}
+    const backgroundImage = view === 'first'
+        ? require('../../../assets/Mesh-gradient.png')
+        : require('../../../assets/Mesh-gradient1.png')
+    const logoWhiteback = require('../../../assets/piefi-logo–withback.png')
+
 
     return (
         <View style={styles.container}>
             <ImageBackground
-                source={image}
-                resizeMode="cover"
-                style={styles.backgroundImage}
-            >
-                <Text>Onboard</Text>
+                source={backgroundImage}
+                resizeMode={"cover"}
+                style={styles.backgroundImage}>
+                <View style={styles.head}>
+                    <Image source={logoWhiteback}/>
+
+                    <Button
+                        style={styles.changeLangBtn}
+                        title={language}
+                        fontSize={14}
+                        fontWeight={'600'}
+                        color={'#fff'}/>
+                </View>
             </ImageBackground>
 
         </View>
@@ -29,13 +42,21 @@ export const Onboard = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // backgroundColor: '#fff',
-
     },
     backgroundImage: {
         flex: 1,
-        height: '100%',
-        width: '100%',
-        // justifyContent: 'center',
+    },
+    head: {
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingVertical: 70,
+        flexDirection: 'row',
+        justifyContent: "space-between",
+    },
+    changeLangBtn: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        borderRadius: 12,
     }
 });
