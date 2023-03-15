60b0b3894cf37378bd1fc1616bd315e42a86fe19
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-15 21:00:24 +0100

    add Main screen to StackNavigation

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StyleSheet, View } from 'react-native'
 
 import { RootStackParamList } from '../types/navigationsTypes'
 
+import { Home } from './Home'
 import { Onboard } from './Onboard/Onboard'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export const Main = () => {
   return (
-    // <View style={styles.container}>
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {/*<Stack.Screen name={'Splash'} component={Splash}/>*/}
-      <Stack.Screen name={'Onboard'} component={Onboard} />
-    </Stack.Navigator>
-    // </View>
+    <View style={styles.container}>
+      <Stack.Navigator initialRouteName={'Onboard'} screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={'Onboard'} component={Onboard} />
+        <Stack.Screen name={'Home'} component={Home} />
+      </Stack.Navigator>
+    </View>
   )
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//   },
-// })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+})
