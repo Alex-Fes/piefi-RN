1eb57d32cb3a777dd68562c3bc7b0da062edd3fa
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-17 21:21:00 +0100

    add FirstFrame component, change styles

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 
-import { View, Text, StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import { FirstFrame } from './firstFrame/FirstFrame'
 
 export const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>First screen</Text>
+      <View style={styles.firstFrame}>
+        <FirstFrame />
+      </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#dfe4f1',
+    // alignItems: 'center',
+    // justifyContent: 'flex-start',
+    // position: 'relative',
+  },
+  firstFrame: {
+    backgroundColor: '#1F1D28',
+    // width: WIDTH,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    // position: 'absolute',
+    // top: 0,
+    // left: 0,
   },
 })
