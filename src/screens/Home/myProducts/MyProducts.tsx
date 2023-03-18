620511602312d28a8057bb4c1164f783bab15b81
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-18 17:28:36 +0100

    refactor MyProducts.tsx, add Product.tsx, add styles

--- src/screens/Home/myProducts/MyProducts.tsx
+++ src/screens/Home/myProducts/MyProducts.tsx
@@ -1,22 +1,73 @@
 import React from 'react'
 
-import { View, Text, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native'
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-import { PADDING } from '../../../constants/constants'
+import { PADDING, WIDTH } from '../../../constants/constants'
+
+import { Product } from './Product'
+import { investment, paymentData, savingBalance } from './productData'
 
 export const MyProducts = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Мои продукты</Text>
+      <View style={styles.titleBox}>
+        <Text style={styles.text}>Мои продукты</Text>
+        <TouchableOpacity
+          style={styles.settingsBtn}
+          onPress={() => {
+            Alert.alert('Go to settings')
+          }}
+        >
+          <Image
+            style={styles.settingsIcon}
+            source={require('../../../../assets/icons/settingsIcon.png')}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.productBox}>
+        <Product
+          bankSystemLogo={paymentData.bankSystemLogo}
+          littleLogo={paymentData.littleLogo}
+          productBalance={paymentData.productBalance}
+          arrow={paymentData.arrow}
+          productTitle={paymentData.productTitle}
+          navigate={paymentData.navigate}
+          cardLogo={paymentData.cardLogo}
+          cardNumber={paymentData.cardNumber}
+          currentCurrency={paymentData.currentCurrency}
+        />
+        <Product
+          bankSystemLogo={savingBalance.bankSystemLogo}
+          littleLogo={savingBalance.littleLogo}
+          productBalance={savingBalance.productBalance}
+          arrow={savingBalance.arrow}
+          productTitle={savingBalance.productTitle}
+          navigate={savingBalance.navigate}
+          cardLogo={savingBalance.cardLogo}
+          currentCurrency={savingBalance.currentCurrency}
+        />
+        <Product
+          bankSystemLogo={investment.bankSystemLogo}
+          littleLogo={investment.littleLogo}
+          productBalance={investment.productBalance}
+          arrow={investment.arrow}
+          productTitle={investment.productTitle}
+          navigate={investment.navigate}
+          cardLogo={investment.cardLogo}
+          currentCurrency={investment.currentCurrency}
+          investProfit={investment.investProfit}
+        />
+      </View>
       <TouchableOpacity
-        style={styles.settingsBtn}
+        style={styles.allProductsBox}
         onPress={() => {
-          Alert.alert('Go to settings')
+          Alert.alert('Watch all products')
         }}
       >
+        <Text style={styles.secondTitle}>Смотреть все</Text>
         <Image
-          style={styles.settingsIcon}
-          source={require('../../../../assets/icons/settingsIcon.png')}
+          style={styles.secondTitleIcon}
+          source={require('../../../../assets/icons/myProductsIcons/rightArrowIcon.png')}
         />
       </TouchableOpacity>
     </View>
@@ -24,8 +75,8 @@ export const MyProducts = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
+  container: {},
+  titleBox: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -38,7 +89,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   settingsBtn: {
-    //backgroundColor: '#646f85',
     width: 40,
     height: 40,
     justifyContent: 'center',
@@ -49,5 +99,26 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     top: 2,
+  },
+  productBox: {
+    width: WIDTH - PADDING * 2,
+    marginHorizontal: PADDING,
+  },
+  allProductsBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 48,
+    borderRadius: 16,
+    marginVertical: 10,
+  },
+  secondTitle: {
+    fontWeight: '600',
+    fontSize: 16,
+    color: '#FFF',
+  },
+  secondTitleIcon: {
+    width: 16,
+    height: 16,
   },
 })
