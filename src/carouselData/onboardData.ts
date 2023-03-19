d794351b4fd75b7fd68088e725006a35b2154ad6
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-19 15:17:43 +0100

    add data for OffersCaarousel

--- src/carouselData/onboardData.ts
+++ src/carouselData/onboardData.ts
@@ -12,3 +12,16 @@ export const carouselItem = [
     url: require('../../assets/onboardSecondImg.png'),
   },
 ]
+
+export const offerDataCarousel = [
+  {
+    description: 'Карта Piefi для оплаты покупок и снятия наличных.',
+    url: require('../../assets/icons/offerIcon.png'),
+    backgroundImg: require('../../assets/Mesh-gradient.png'),
+  },
+  {
+    description: 'Инвестируйте в криптовалюту с доходностью от 10%.',
+    url: require('../../assets/onboardSecondImg.png'),
+    backgroundImg: require('../../assets/Mesh-gradient1.png'),
+  },
+]
