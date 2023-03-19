2ddab56cefe806e94f4bae115d31ef5d46e8b3ac
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-19 17:37:47 +0100

    add currentCurrencyData for ExchangeRate component

--- src/carouselData/onboardData.ts
+++ src/carouselData/onboardData.ts
@@ -15,13 +15,40 @@ export const carouselItem = [
 
 export const offerDataCarousel = [
   {
-    description: 'Карта Piefi для оплаты покупок и снятия наличных.',
+    description: 'Карта Piefi для оплаты покупок и снятия наличных ',
     url: require('../../assets/icons/offerIcon.png'),
     backgroundImg: require('../../assets/Mesh-gradient.png'),
   },
   {
-    description: 'Инвестируйте в криптовалюту с доходностью от 10%.',
+    description: 'Инвестируйте в криптовалюту с доходностью от 10%',
     url: require('../../assets/onboardSecondImg.png'),
     backgroundImg: require('../../assets/Mesh-gradient1.png'),
+  },
+]
+
+export const currentCurrencyData = [
+  {
+    currencyIndex: 'GBR',
+    currencyName: 'Фунт стерлингов',
+    exchangeValue: '1.14',
+    exchangeCurrency: 'EUR',
+  },
+  {
+    currencyIndex: 'USD',
+    currencyName: 'Доллар США',
+    exchangeValue: '0.89',
+    exchangeCurrency: 'EUR',
+  },
+  {
+    currencyIndex: 'BTC',
+    currencyName: 'Bitcoin',
+    exchangeValue: '21 090.44',
+    exchangeCurrency: 'EUR',
+  },
+  {
+    currencyIndex: 'ETH',
+    currencyName: 'Ethereum',
+    exchangeValue: '1 474.21',
+    exchangeCurrency: 'EUR',
   },
 ]
