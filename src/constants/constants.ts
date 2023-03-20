8a0110c5cf6ef8207797a551a1bf79955724e4d2
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-03-20 10:41:00 +0100

    create constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,3 +6,15 @@ export const WIDTH = width
 export const HEIGHT = height
 export const PADDING = 15
 export const NUM_COLUMN = 2
+
+export const BG_COLOR_LIGHT = '#FFFFFF'
+export const BG_COLOR_DARK = '#16151E'
+export const BG_COLOR_SECOND = '#1F1D28'
+export const BG_COLOR_FOR_ITEM = '#F4F4F4'
+export const BG_COLOR_FOR_ITEM_DARK = '#302A3B'
+export const BG_COLOR_FOR_TAB_NAVIGATION = 'rgba(255, 255, 255, 0.9)'
+export const BG_COLOR_FOR_TAB_NAVIGATION_DARK = 'rgba(22, 21, 30, 0.9)'
+export const TEXT_COLOR = '#FFFFFF'
+export const TEXT_COLOR_SECOND = '#221C35'
+export const TINTCOLOR = '#8854FA'
+export const TINTCOLOR_SECOND = '#b8b6be'
